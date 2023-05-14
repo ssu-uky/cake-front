376d76fbe19f96f5877b8e2ae1b5aa3c1805328e
ssu-uky <id_suhyun@naver.com>
2023-05-15 06:50:13 +0900

    modal

--- pages/caketables/[user_pk].jsx
+++ pages/caketables/[user_pk].jsx
@@ -2,6 +2,7 @@ import css from "styled-jsx/css";
 import Image from "next/image";
 import Caketable from "public/images/Caketable.png";
 
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "../components/Sidebar";
@@ -14,9 +15,8 @@ export default function Main() {
   const router = useRouter();
   const { user_pk } = router.query;
   const [cakeData, setCakeData] = useState({});
-  const [showModal, setShowModal] = useState(false);
-  const [selectedCake, setSelectedCake] = useState(null);
   const [loggedInUserPk, setLoggedInUserPk] = useState(null);
+
 
   // 로그인한 사용자의 user_pk를 가져오는 함수 추가
 const fetchLoggedInUserPk = async () => {
@@ -36,11 +36,11 @@ const fetchLoggedInUserPk = async () => {
   }
   try {
     const response = await fetch("http://127.0.0.1:8000/api/users/login/token/refresh/", {
-    // const response = await fetch("http://127.0.0.1:8000/api/auth/user/", {
+    // const response = await fetch("http://127.0.0.1:8000/api/token/", {
+    // const response = await fetch("https://kauth.kakao.com/oauth/token", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${accessToken}`,
         Authorization: `Bearer ${validAccessToken}`,
       },
     });
@@ -70,7 +70,8 @@ const isTokenExpired = (token) => {
   // 새로운 access_token을 발급받는 함수
 const getNewAccessToken = async (refreshToken) => {
   try {
-    const response = await fetch("http://127.0.0.1:8000/api/users/login/token/refresh/", {
+    // const response = await fetch("http://127.0.0.1:8000/api/users/login/token/refresh/", {
+    const response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -85,10 +86,6 @@ const getNewAccessToken = async (refreshToken) => {
     return null;
   }
 };
-
-
-
-
 
 // 컴포넌트가 마운트되면 로그인한 사용자의 user_pk를 가져옵니다.
 useEffect(() => {
@@ -122,6 +119,7 @@ useEffect(() => {
   };
   // 페이지 네이션 구현 끝 (하단에서 버튼으로 다시 이어짐)
 
+
   // 링크 복사
   const copyURL = async () => {
     try {
@@ -134,12 +132,14 @@ useEffect(() => {
   };
   // 링크 복사 끝
 
+
   // 하단 버튼 시작
   const handleClick = (event) => {
     event.preventDefault();
     router.push(`/caketables/${cakeData.user_pk}/cake/`);
   };
   // 하단 버튼 끝
+
 
   //user 정보
   useEffect(() => {
@@ -157,7 +157,6 @@ useEffect(() => {
       })
       .then((data) => {
         setCakeData(data[0]);
-        console.log(data);
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -169,41 +168,28 @@ useEffect(() => {
     backgroundColor: tableColor, marginTop:"20px" 
   };
 
-  const handleImageSelection = (pickcake) => {
-    setSelectedCake(pickcake);
+
+  // 모달 시작 2 // 
+  const [showModal, setShowModal] = useState(false);
+  const [selectedCake, setSelectedCake] = useState(null);
+  const [selectedVisitor, setSelectedVisitor] = useState(null);
+
+  // 모달 컨테이너 클릭 이벤트 핸들러
+
+  const handleShowModal = (event, visitor) => {
+    event.preventDefault();
+    setSelectedCake(visitor.pickcake);
+    setSelectedVisitor(visitor);
+    setShowModal(true);
   };
 
-    // 모달 시작 (클릭 시 편지와 보이는 케이크)
-  // const handleShowModal = (event, pickcake) => {
-  //   event.stopPropagation();
-  //   setSelectedCake(pickcake);
-  //   setShowModal(true);
-  // };
+  const handleHideModal = () => {
+    setSelectedCake(null);
+    setSelectedVisitor(null);
+    setShowModal(false);
+  };
 
-  const handleShowModal = (event, pickcake) => {
-    event.stopPropagation();
-
-    // 로그인한 사용자와 테이블 주인이 일치하는지 확인
-    if (loggedInUserPk && loggedInUserPk === user_pk) {
-    setSelectedCake(pickcake);
-    setShowModal(true);
-  } else {
-    alert("테이블 주인만 편지 내용을 확인할 수 있습니다.");
-  }
-};
-//   const loggedInUser = sessionStorage.getItem("user_pk");
-//   if (loggedInUser && loggedInUser === user_pk) {
-//     setSelectedCake(pickcake);
-//     setShowModal(true);
-//   } else {
-//     alert("테이블 주인만 편지 내용을 확인할 수 있습니다.");
-//   }
-// };
-
-
-  const handleHideModal = (e) => setShowModal(false);
-  // 모달 끝
-
+  // 모달 끝 2 //
   
   return (
     <div className="main_container bgimg">
@@ -215,11 +201,12 @@ useEffect(() => {
 
       <div className="main_cakeImg">
         {/* 페이지네이션 버튼 구현 (1줄에 4개, 총 2줄) */}
-        {paginatedCakes[currentPage] && paginatedCakes[currentPage].map((visitor, index) => (
-          <div className={`pickcake ${index < 4 ? "first-row" : "second-row"}`}
-            key={index}
-            onClick={(event) => handleShowModal(event, visitor)}
-          >
+        {paginatedCakes[currentPage] &&
+          paginatedCakes[currentPage].map((visitor, index) => (
+            <div className={`pickcake ${index < 4 ? "first-row" : "second-row"}`}
+              key={index}
+              onClick={(event) => handleShowModal(event, visitor)}
+            >
             {visitor.pickcake === selectedCake ? (
               <Image
                 src={`/images/cakes/${selectedCake}.png`}
@@ -239,18 +226,38 @@ useEffect(() => {
                   priority
               />
             )}
-            {showModal && (
-              <div className="modal">
-                <span className="close" onClick={handleHideModal}>
-                  &times;
-                </span>
-                <p id={visitor.id}>{visitor.visitor_name}</p>
-                <p id={visitor.id}>{visitor.letter}</p>
+
+            {/* 모달 창 코드 시작 */}
+            {/* {showModal && visitor.pickcake === selectedCake && ( */}
+            {selectedVisitor === visitor && (
+              <div
+                className="modal_container"
+                onClick={handleHideModal}
+                >
+                <div className="modal_inner">
+                  <span
+                    className="modal_close"
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      handleHideModal();
+                    }}
+                    style={{ cursor: "pointer" }}
+                  >
+                    닫기&nbsp; &times;
+                  </span>
+                  <p className="modal_title"  id={selectedCake}> {visitor.visitor_name} </p>
+                <br></br>
+                  <p className="modal_body"  id={selectedCake}> {visitor.letter} </p>
+              </div>
               </div>
             )}
+
+            {/* 모달 창 코드 끝 */}
+
           </div>
         ))}
       </div>
+      
       
       <div style={style}>
         <Image src={Caketable} alt="caketableimg" width={500} height={450} className="caketable" />
@@ -263,7 +270,7 @@ useEffect(() => {
         style={{
           fontSize: '1.5em',
           marginRight: '30px',
-          marginTop: '25px',
+          marginTop: '30px',
           cursor: currentPage === 0 ? 'default' : 'pointer',
           opacity: currentPage === 0 ? 0.5 : 1,
           color: currentPage === 0 ? '' : 'white',
@@ -278,7 +285,7 @@ useEffect(() => {
         style={{
           fontSize: '1.5em',
           marginLeft: '30px',
-          marginTop: '25px',
+          marginTop: '30px',
           cursor: currentPage === paginatedCakes.length - 1 ? 'default' : 'pointer',
           opacity: currentPage === paginatedCakes.length - 1 ? 0.5 : 1,
           color: currentPage === paginatedCakes.length - 1 ? '' : 'white',
@@ -305,7 +312,6 @@ useEffect(() => {
   );
 }
 
-
 const main = css`
   @font-face {
     font-family: "Bazzi";
@@ -314,60 +320,55 @@ const main = css`
     font-weight: normal;
     font-style: normal;
   }
-
+  
   .main_container {
-    width: 100%;
+    width: 500px;
     height: 100vh;
     overflow: hidden;
     background-color: #f7bedf;
     color: white;
     text-align: center;
-    font-family: "Bazzi";
     position: relative;
-    
+    font-family: "Bazzi";
+    vertical-align: middle;
+
     //중앙정렬
-    // position: absolute;
+    position: absolute;
     top: 50%;
-    // left: 50%;
-    // transform: translate(-50%, -50%);
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 
-
   .main_text {
-    top: 50px;
     font-size: 30px;
-    margin-bottom: 30px;
-    line-height: 0px;
+    margin-bottom: 35px;
+    line-height: 5px;
     margin-top: 40px;
   }
 
   // 하단 버튼 정렬
   .main_btn_container {
-    width: 100%;
-    position: absolute;
     display: flex;
-    margin-top: 10%;
-    align-items: center;
     justify-content: space-evenly;
+    margin-top: 30px;
   }
   
   // 하단 버튼 디자인
   .main_btn {
+    width: 150px;
     height: 45px;
     border-radius: 15px;
     border: none;
     font-family: "Bazzi";
     background-color: #f073cd;
     color: white;
-    font-size: 14px;
-    cursor: pointer;
-    justify-content: space-evenly;
+    font-size: 15px;
   }
-  
 
   // 케이크 테이블 이미지
   .caketableimg {
     position: relative;
+    width: 100%;
     display: flex;
     top: 100px;
     // left: 50%;
@@ -380,20 +381,78 @@ const main = css`
     display: flex;
     flex-flow: row wrap;
     width: 100%;
-    top: 330px;
+    top: 340px;
+    justify-content: space-between;
+    z-index: 10;
   }
 
   .pickcake {
-    display: inline-block;
     width: calc(25% - 10px);
-    margin: 5px;
-    // height: 100px;
-    text-align: center;
-    line-height: 10px;
-    z-index: 50;
+    height: 100px;
+    display: flex;
+    justify-content: center;
   }
 
+  // 두번째 줄
   .second-row {
-    margin-top: 20px;
+    margin-top: 25px;
+  }
+
+  // 모달 창
+  .modal_container{
+    width: 100%;
+    height: 350px;
+    border-radius: 20px;
+    font-family: "Bazzi";
+    background-color: rgba(247,191,224, 0.8);
+    // backdrop-filter: blur(1px);
+    // color: black;
+    color: f073cd;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    padding: 20px;
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    // align-items: flex-end;
+}
+
+  .modal_inner{
+    width : 90%;
+    height : 70%;
+    display: absolute;
+    margin : 0 auto;
+    vertical-align: middle;
+    align-items: center;
+    background-color: #f073cd;
+    // background-color: white;
+    border-radius: 20px;
+    margin-top: 50px;
+    // margin-bottom: 35px;
+  }
+
+  .modal_close {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    color: white;
+    font-size: 17px;
+  }
+
+  .modal_title {
+    // color: #f073cd;
+    font-size: 20px;
+    margin: 0 auto;
+    padding-top: 40px;
+    text-align: center;
+  }
+
+  .modal_body {
+    // color: #f073cd;
+    font-size: 17px;
+    padding: 10px;
+    text-align: center;
   }
 `;
+
